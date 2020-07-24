ba34cc8200737550acb210de14694b5bd4a673a7
HanselD <hansel96@gmail.com>
2020-07-24 03:20:35 -0400

    should be working

--- code/app.py
+++ code/app.py
@@ -6,27 +6,35 @@ import os
 dynamo = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamo.Table(os.environ['tablename'])
 
+
 def lambda_handler(event, context):
-    
+
     try:
-        #get current value from ddb
-        resp = table.get_item(Key={'id':1})        
+        # get current value from ddb
+        resp = table.get_item(Key={'id': 1})
         current = resp['Item']['visitors']
 
-        #add 1
+        # add 1
         current += 1
-        
-        #update ddb
+
+        # update ddb
         table.update_item(
-            Key={'id':1},
+            Key={'id': 1},
             UpdateExpression='SET visitors = :val1',
             ExpressionAttributeValues={':val1': current}
         )
 
-        #return to website        
+        # return to website
         return {
-            'count': current,
-            'statusCode': 200
+            "body": json.dumps({
+                'count': current, }),
+            'statusCode': 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "https://www.hdsouza.com",
+                "Access-Control-Allow-Headers": "application/json",
+                'Access-Control-Allow-Methods': 'OPTIONS,POST',
+                "Access-Control-Allow-Credentials": 'true'
+            },
         }
 
     except Exception as e:
